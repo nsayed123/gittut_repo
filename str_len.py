ca21a8073003414e6a514ebe30ce8d2aa403d598
sayedsabirali <sayedsabirali096@gmail.com>
2018-06-21 21:53:11 +0530

    reverting back the changes of last commit

--- str_len.py
+++ str_len.py
@@ -1,2 +1,4 @@
 str_name = "Happiness radiates like the fragrance from a flower and draws all good things towards you"
+print("Welcome")
 print("length of the string is "+str(len(str_name)))
+print("Thank you")
